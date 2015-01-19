96ce03c737b2ddb20dcae0b1215b47dc2f05bc7e
Kevin Kwok <antimatter15@gmail.com>
2015-01-19 09:47:50 -0500

    first commit

---
+++ helpers.js
@@ -0,0 +1,80 @@
+
+function assert(x){
+    if(!x) throw "Assertion failure";
+}
+
+function nanfree(m){
+    for(var i = 0; i < m.length; i++){
+        if(m[i].length){
+            // console.log(i, m[i].length)
+            for(var j = 0; j < m[i].length; j++){
+                if(isNaN(m[i][j])) throw "found a NaN";
+            }
+        }else if(isNaN(m[i])){
+            throw "found a NaN"
+        }
+    }
+}
+
+
+// NOTE: this is an ES6 function, so we should ship a polyfill
+// http://jsperf.com/hyperbolic-tangent-2
+// http://jsperf.com/hyperbolic-tangent
+// There's actually also the chance that a pure-js solution
+// ends up faster than the native one because we don't
+// have to deal with the edge case of +/- Inf
+function tanh(x) {
+    // it's important to clip it because javascript's
+    // native Math.tanh returns NaN for values of x
+    // whose absolute value is greater than 400
+    var y = Math.exp(2 * Math.max(-100, Math.min(100, x)));
+    return (y - 1) / (y + 1);
+}
+
+// set a vector as the elementwise product of two others
+function setmulvec(dest, a, b){
+    for(var i = a.length; i--;) dest[i] = a[i] * b[i];
+}
+
+// add the elementwise product of two vectors to another
+function addmulvec(dest, a, b){
+    for(var i = a.length; i--;) dest[i] += a[i] * b[i];
+}
+
+// create a 2d matrix
+function mat(r, c){
+    var arr = [];
+    for(var i = 0; i < r; i++) arr[i] = new Float32Array(c);
+    return arr;
+}
+
+// clear a 2d matrix
+function rmat(m){
+    for(var i = 0; i < m.length; i++)
+        for(var j = 0; j < m[i].length; j++)
+            m[i][j] = NaN;
+}
+
+
+function array_max(n){
+    var m = n[0], b = 0;
+    for(var i = 1; i < n.length; i++)
+        if(n[i] > m) m = n[b = i];
+    return b;
+}
+
+
+function array_split(arr, test){
+    if(!test) test = function(a, b) { return a == b };
+    var buf = [arr[0]],
+        groups = [];
+    for(var i = 1; i < arr.length; i++){
+        if(!test(arr[i - 1], arr[i])){
+            if(buf.length) groups.push(buf);
+            buf = [];
+        }
+        buf.push(arr[i])
+    }
+    if(buf.length) groups.push(buf);
+    return groups
+}
---
+++ lstm.js
@@ -0,0 +1,190 @@
+/*
+ * This is a port of Thomas M. Breuel's lstm.py from OCROpus to Javascript
+ * It's meant to be compatible with parameters trained with OCROpus' pyrnn
+ * and clstm, so I've only implemented the methods for forward propagation.
+ *
+ * Authors: Kevin Kwok <antimatter15@gmail.com>
+ *          Thomas M. Breuel (OCROpus and clstm)
+ *
+ * License: MIT License
+ */
+
+// Nonlinearity used for gates.
+function ffunc(vec){
+    var arr = new Float32Array(vec.length);
+    for(var i = 0; i < vec.length; i++)
+        arr[i] = 1.0 / (1.0 + Math.exp(-vec[i]));
+    return arr;
+}
+
+// Nonlinearity used for input to state.
+function gfunc(vec){
+    var arr = new Float32Array(vec.length);
+    for(var i = 0; i < vec.length; i++) arr[i] = tanh(vec[i]);
+    return arr;
+}
+// Nonlinearity used for output
+function hfunc(vec){
+    var arr = new Float32Array(vec.length);
+    for(var i = 0; i < vec.length; i++) arr[i] = tanh(vec[i]);
+    return arr;
+}
+
+function LSTM(Ni, Ns) {
+    this.Ni = Ni;
+    this.Ns = Ns;
+    this.Na = Ni + Ns + 1;
+    this.allocate(5000)
+    this.reset()
+}
+
+LSTM.prototype.allocate = function(maxlen){
+    this.cix    = mat(maxlen, this.Ns) 
+    this.ci     = mat(maxlen, this.Ns) 
+    this.gix    = mat(maxlen, this.Ns) 
+    this.gi     = mat(maxlen, this.Ns) 
+    this.gox    = mat(maxlen, this.Ns) 
+    this.go     = mat(maxlen, this.Ns) 
+    this.gfx    = mat(maxlen, this.Ns) 
+    this.gf     = mat(maxlen, this.Ns)
+    this.state  = mat(maxlen, this.Ns)
+    this.output = mat(maxlen, this.Ns)
+    this.source = mat(maxlen, this.Na)
+}
+
+LSTM.prototype.reset = function(){
+    rmat(this.cix); rmat(this.ci)
+    rmat(this.gix); rmat(this.gi)
+    rmat(this.gox); rmat(this.go)
+    rmat(this.gfx); rmat(this.gf)
+    rmat(this.source)
+    rmat(this.state)
+    rmat(this.output)
+}
+
+LSTM.prototype.forward = function(xs){
+    // assert(xs[0].length == this.Ni)
+    var n = xs.length,
+        N = this.gi.length;
+    if(n > N) throw "input too large for LSTM model";
+    this.reset();
+    var prev = new Float32Array(this.Ns)
+    for(var t = 0; t < n; t++){
+        // assert(xs[t].length == this.Ni)
+        this.source[t][0] = 1
+        this.source[t].set(xs[t], 1)
+        this.source[t].set(prev, 1 + this.Ni)
+        for(var j = 0; j < this.Ns; j++){
+            this.gix[t][j] = 0; 
+            this.gfx[t][j] = 0; 
+            this.gox[t][j] = 0; 
+            this.cix[t][j] = 0; 
+            for(var k = 0; k < this.Na; k++){
+                var sk = this.source[t][k];
+                this.gix[t][j] += this.WGI[j][k] * sk
+                this.gfx[t][j] += this.WGF[j][k] * sk
+                this.gox[t][j] += this.WGO[j][k] * sk
+                this.cix[t][j] += this.WCI[j][k] * sk
+            }
+        }
+        if(t > 0){
+            addmulvec(this.gix[t], this.WIP, this.state[t-1])
+            addmulvec(this.gfx[t], this.WFP, this.state[t-1])
+        }
+        this.gi[t] = ffunc(this.gix[t])
+        this.gf[t] = ffunc(this.gfx[t])
+        this.ci[t] = gfunc(this.cix[t])
+        setmulvec(this.state[t], this.ci[t], this.gi[t]);
+        if(t > 0){
+            addmulvec(this.state[t], this.gf[t], this.state[t-1])
+            addmulvec(this.gox[t], this.WOP, this.state[t])
+        }
+        this.go[t] = ffunc(this.gox[t])
+        setmulvec(this.output[t], hfunc(this.state[t]), this.go[t])
+        prev = this.output[t];
+    }
+    // nanfree(this.output.slice(0, n))
+    return this.output.slice(0, n)
+}
+
+function Stacked(nets){ this.nets = nets }
+
+Stacked.prototype.forward = function(xs) {
+    for(var i = 0; i < this.nets.length; i++)
+        xs = this.nets[i].forward(xs);
+    return xs;
+}
+
+function Reversed(net){ this.net = net }
+Reversed.prototype.forward = function(xs) {
+    return this.net.forward(xs.slice(0).reverse()).slice(0).reverse();
+};
+
+function Parallel(nets){ this.nets = nets }
+Parallel.prototype.forward = function(xs) {
+    var a = this.nets[0].forward(xs),
+        b = this.nets[1].forward(xs);
+    var m = mat(a.length, a[0].length + b[0].length)
+    for(var i = 0; i < a.length; i++){
+        m[i].set(a[i])
+        m[i].set(b[i], a[0].length)
+    }
+    return m;
+}
+
+function Softmax(Nh, No){
+    this.Nh = Nh;
+    this.No = No;
+}
+
+Softmax.prototype.forward = function(ys){
+    var n  = ys.length,
+        zs = [];
+    for(var i = 0; i < n; i++){
+        var temp = new Float32Array(this.No),
+            total = 0;
+        for(var j = 0; j < this.No; j++){
+            var v = this.B[j]
+            for(var k = 0; k < this.Nh; k++){
+                v += this.W[j][k] * ys[i][k]
+            }
+            temp[j] = Math.exp(Math.min(100, Math.max(-100, v)))
+            total += temp[j]
+        }
+        for(var j = 0; j < this.No; j++) temp[j] /= total;
+        zs[i] = temp;
+    }
+    return zs;
+}
+
+// A bidirectional LSTM constructed from regular and reversed LSTMs.
+function BIDILSDM(Ni, Ns, No){
+    var lstm1 = new LSTM(Ni, Ns),
+        lstm2 = new Reversed(new LSTM(Ni, Ns)),
+        bidi  = new Parallel([lstm1, lstm2]),
+        soft  = new Softmax(2 * Ns, No),
+        stack = new Stacked([bidi, soft]);
+    return stack;
+}
+
+function SequenceRecognizer(ninput, nstates, noutput) {
+    this.Ni   = ninput;
+    this.Ns   = nstates;
+    this.No   = noutput;
+    this.lstm = BIDILSDM(this.Ni, this.Ns, this.No);
+}
+
+// predict an integer sequence of codes
+SequenceRecognizer.prototype.predictSequence = function(xs) {
+    if(xs[0].length != this.Ni) throw "wrong image height";
+    this.output = this.lstm.forward(xs);
+
+    return array_split(this.output
+        .map(x=>Math.max.apply(Math, x) > 0.6 && x)
+        .filter(x=>x)
+        .map(x=>array_max(x)))
+};
+
+SequenceRecognizer.prototype.predictString = function(xs) {
+    return this.predictSequence(xs).map(x=>code2char[x[0]]).join('')
+};
